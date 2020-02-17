5e139fdcb05cf44204a8e3485d6e201a7212e65d
ace113 <prajwalmaharjan58@gmail.com>
2020-02-17 14:22:27 +0545

    make it better'

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const passport = require('passport')
 const flash = require('connect-flash')
 require('./passport')
 
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 
 
 // env config
@@ -18,7 +21,7 @@ dotenv.config();
 
 
 // connect mongodb
-const URL = 'mongodb://localhost:27017/herbshop';
+const URL = process.env.DATABASE_URI;
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Database is ready'))
     .catch(err => console.log(err))
